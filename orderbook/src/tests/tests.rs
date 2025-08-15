438285846545563130a2feb62343337334850b3a
prranavv <pranavvkumar03@gmail.com>
2025-08-15 22:29:52 +0530

    added tests for spread and midprice

--- orderbook/src/tests/tests.rs
+++ orderbook/src/tests/tests.rs
@@ -50,3 +50,39 @@ fn test_get_best_bid(){
     assert_ne!(orderbook.get_best_bid(),Some(&Reverse(limit_order_3.price)));
     assert_eq!(orderbook.get_best_bid(),Some(&Reverse(limit_order_2.price)));
 }
+
+#[test]
+fn test_get_spread(){
+    let mut orderbook = Orderbook::new();
+    let limit_order = LimitOrder{price:dec!(105),quantity:dec!(200),side:Side::Asks,user_id:1};
+    orderbook.add_limit_order(limit_order.clone());
+    let limit_order_2 = LimitOrder{price:dec!(104),quantity:dec!(200),side:Side::Bids,user_id:1};
+    orderbook.add_limit_order(limit_order_2.clone());
+
+    let result = orderbook.get_spread();
+    assert_eq!(result,Some(dec!(1)));
+
+    drop(orderbook);
+
+    let orderbook = Orderbook::new();
+    let result = orderbook.get_spread();
+    assert_eq!(result,None);
+}
+
+#[test]
+fn test_get_mid_price(){
+    let mut orderbook = Orderbook::new();
+    let limit_order = LimitOrder{price:dec!(105),quantity:dec!(200),side:Side::Asks,user_id:1};
+    orderbook.add_limit_order(limit_order.clone());
+    let limit_order_2 = LimitOrder{price:dec!(104),quantity:dec!(200),side:Side::Bids,user_id:1};
+    orderbook.add_limit_order(limit_order_2.clone());
+
+    let result = orderbook.mid_price();
+    assert_eq!(result,Some(dec!(104.5)));
+
+    drop(orderbook);
+
+    let orderbook = Orderbook::new();
+    let result = orderbook.get_spread();
+    assert_eq!(result,None);
+}
